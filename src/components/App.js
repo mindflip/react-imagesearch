0fe677c40f6802ecc005702cea8b5f4147009b02
Eomy <hmu1212@hotmail.com>
2022-02-03 01:06:56 +0900

    [feat] searchbar input

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,16 @@ import React from "react";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  state = { images: [] };
+
+  onSearchSubmit = (term) => {
+    console.log(term);
+  };
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
